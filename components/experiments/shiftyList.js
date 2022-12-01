2551c98d0c04c0b3bff1bd1e3ac65b5d2dbad65b
SLocke <slocke@go2boss.com>
2022-12-01 08:42:51 -0500

    tweaked properties of the ShiftyList

--- components/experiments/shiftyList.js
+++ components/experiments/shiftyList.js
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { Flipped, Flipper } from "react-flip-toolkit";
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpwardRounded';
 import { ArrowDownward } from "@mui/icons-material";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 
 const ShiftyList = ({ randos }) => {
 	const [data, setData] = useState(randos)
@@ -38,57 +39,85 @@ const ShiftyList = ({ randos }) => {
 			}}>
 				<Flipper flipKey={data}>
 					<button onClick={shuffleList}>Shuffle</button>
-					<Stack spacing={1} sx={{ padding: '1rem' }}>
-						{
-							data.map((item, i) => {
-								return (
-									<Flipped
-										flipId={item.id}
-										key={i}
-										spring={'wobbly'}
-										stagger={true}
-									>
-										<Card display='flex' raised sx={{ p: 1 }} >
-											<Typography sx={{
-												fontWeight: 'bold',
-												display: 'inline-flex'
-											}}
-											>Name: </Typography>
-											<Typography sx={{ display: 'inline-flex' }}>{item.name}</Typography>
-											<Typography sx={{
-												fontWeight: 'bold',
-												display: 'inline-flex'
-											}}
-											>ID: </Typography>
-											<Typography sx={{ display: 'inline-flex' }}>{item.id}</Typography>
-											<ButtonGroup
-												variant="contained"
-												size="small"
-												color="primary"
-												sx={{ float: 'right' }}
-											>
-												<Button id='up' ind={i} onClick={arrowClickHandler}>
-													<ArrowUpwardIcon />
-												</Button>
-												<Button id='down' ind={i} onClick={arrowClickHandler}>
-													<ArrowDownward />
-												</Button>
-											</ButtonGroup>
-											<Typography
-												variant="h6"
-												sx={{
-													marginRight: '1rem',
-													float: 'right'
-												}}
-											>
-												Rank: {i + 1}
-											</Typography>
-										</Card>
-									</Flipped>
-								)
-							})
-						}
-					</Stack>
+					<Typography>
+						<Stack spacing={1} sx={{ padding: '1rem' }}>
+							{
+								data.map((item, i) => {
+									return (
+										<Flipped
+											flipId={item.id}
+											key={i}
+											spring={'wobbly'}
+											stagger={true}
+										>
+											<Card display='flex' raised sx={{ p: 1 }} >
+												<Grid2 xs container>
+													<Grid2 xs={6}>
+														<Stack>
+															<Box>
+																<Typography
+																	variant="h6"
+																	sx={{ display: 'inline-flex' }}
+																>
+																	Name:
+																</Typography>
+																<Typography sx={{
+																	display: 'inline-flex',
+																	marginLeft: '1rem'
+																}}
+																>{item.name}
+																</Typography>
+															</Box>
+															<Box>
+																<Typography
+																	variant="h6"
+																	sx={{ display: 'inline-flex' }}
+																>
+																	ID:
+																</Typography>
+																<Typography sx={{
+																	display: 'inline-flex',
+																	marginLeft: '3.25rem'
+																}}
+																>{item.id}
+																</Typography>
+															</Box>
+														</Stack>
+													</Grid2>
+													<Grid2 xs={6} sx={{m:'auto'}}>
+														<Box>
+															<ButtonGroup
+																variant="contained"
+																size="small"
+																color="primary"
+																sx={{ float: 'right' }}
+															>
+																<Button id='up' ind={i} onClick={arrowClickHandler}>
+																	<ArrowUpwardIcon />
+																</Button>
+																<Button id='down' ind={i} onClick={arrowClickHandler}>
+																	<ArrowDownward />
+																</Button>
+															</ButtonGroup>
+															<Typography
+																variant="h6"
+																sx={{
+																	marginRight: '1rem',
+																	float: 'right'
+																}}
+															>
+																Rank: {i + 1}
+															</Typography>
+														</Box>
+													</Grid2>
+												</Grid2>
+											</Card>
+										</Flipped>
+									)
+								})
+							}
+						</Stack>
+					</Typography>
 				</Flipper>
 			</Box>
 		</Container>
